0a6e541eaf7886a91393c4940297a76b3086b834
Khenan <52697992+Khenan@users.noreply.github.com>
2022-06-20 16:23:35 +0200

    Update FMOD

--- Assets/Scripts/UI/SplashScreen.cs
+++ Assets/Scripts/UI/SplashScreen.cs
@@ -1,11 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SplashScreen : MonoBehaviour
 {
-    private void Update()
-    {
     
-    }
 }
