eb669f5ddb07d2ae4eb730bdd244c7001aa07955
Edouard-2 <73876708+Edouard-2@users.noreply.github.com>
2022-06-20 16:23:03 +0200

    Update Input Actions

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,22 +9,38 @@ public class PlayerController : MonoBehaviour
     
     //--------------------------Controller----------------------------//
     public float m_speedMovement = 10;
+    public Vector2 m_move;
     
     //-------------------------Input System-----------------------------//
-    public InputAction m_movementActions;
-    public InputAction m_interactAction;
+    private PlayerControls m_controls;
     
     //---------------------------Private---------------------------//
     private Rigidbody2D m_rb;
+
     
     private void Awake()
     {
         m_rb = GetComponent<Rigidbody2D>();
+
+        m_controls = new PlayerControls();
+
+        m_controls.Player.Movement.performed += ctxt => m_move = ctxt.ReadValue<Vector2>();
+        m_controls.Player.Movement.canceled += ctxt => m_move = Vector2.zero;
+    }
+
+    private void OnEnable()
+    {
+        m_controls.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        m_controls.Player.Disable();
     }
 
     public void DoUpdate()
     {
-        Debug.Log(m_movementActions.ReadValue<Vector2>());
-        m_rb.MovePosition( m_rb.position + m_movementActions.ReadValue<Vector2>() * m_speedMovement * Time.deltaTime);
+        Debug.Log(m_move);
+        m_rb.MovePosition( m_rb.position + m_move * m_speedMovement * Time.deltaTime);
     }
 }
