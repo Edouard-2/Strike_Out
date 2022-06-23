90ed36c38fa98408870a7f663c863b6dfd567db3
Edouard-2 <73876708+Edouard-2@users.noreply.github.com>
2022-06-23 18:28:21 +0200

    Update Merge

---
+++ Assets/Scripts/UI/Menu Buttons/GameToSponsorButton.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+public class GameToSponsorButton : MenuButton
+{
+    public override void Interact()
+    {
+        DataManager.Instance.m_masterPlayerList.ForEach(p=>{Destroy(p);});
+        DataManager.Instance.m_masterPlayerList = new List<MasterPlayerController>();
+        
+        SceneManager.Instance.GoToScene(3);
+    }
+}
